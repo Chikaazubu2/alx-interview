4872d64492ff35f9038acb700cf9eb760bd40313
chika goodluck nwaigwe <chikagnwaigwe@gmail.com>
2023-10-19 14:43:10 +0000

    log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,29 +1,41 @@
 #!/usr/bin/python3
-"""Log parsing reads a stdin line by line and computes metrics"""
 
-import fileinput
-from typing import List
+"""
+Script that reads stdin line by line and compute metrics
+After every 10mins of keyboard interrupt, print stats from
+the beginning
+"""
 
+import sys
 
-line_count = 0
 total_size = 0
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
+s_code = 0
 
-for line in fileinput.input():
-    ln = line.split()
 
-    if len(ln) != 9:
-        continue
+def to_print(status_codes, total_size):
+    print(f"File size: {total_size}")
+    for k, v in sorted(status_codes.items()):
+        if v != 0:
+            print(f"{k}: {v}")
 
-    line_count += 1
-    total_size += int(ln[8])
 
-    if int(ln[7]) in status_codes.keys():
-        status_codes[int(ln[7])] += 1
-    if line_count == 10:
-        print("File size: {}".format(total_size))
-        for key in sorted(status_codes.keys()):
-            print("{}: {}".format(key, status_codes[key]))
+try:
+    for line in sys.stdin:
+        line_parts = line.split()
 
-        line_count = 0
-        total_size = 0
+        if len(line_parts) >= 8:
+            s_code = int(line_parts[-2])
+            total_size += int(line_parts[-1])
+
+            if s_code in status_codes:
+                status_codes[s_code] += 1
+
+            line_count += 1
+            if line_count == 10:
+                to_print(status_codes, total_size)
+                line_count = 0
+
+finally:
+    to_print(status_codes, total_size)
